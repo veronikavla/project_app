ea01ca80c5f78218bfe2389af27629df938a5b5e
anna-pokorna <annie.malecka@gmail.com>
2025-05-22 14:46:10 +0200

    Rozšíření na multipage, nová stránka s postupy

---
+++ pages/Postup receptu.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Postup receptu", page_icon="🍲")
+
+# Načtení dat
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/recepty.csv")
+    return df
+
+df = load_data()
+
+# Výběr receptu
+st.title("Pracovní postup receptu")
+recepty = df["nazev_recept"].tolist()
+vybrany_recept = st.sidebar.selectbox("Vyber recept", recepty)
+
+# Zobrazení informací
+if vybrany_recept:
+    data = df[df["nazev_recept"] == vybrany_recept].iloc[0]
+
+    st.header(vybrany_recept.capitalize())
+    
+    #st.markdown(
+    #f"### [{vybrany_recept.capitalize()}]({data['url_recept']})",
+    # unsafe_allow_html=True)
+
+    # Obrázek receptu
+    st.image(data["url_obrazek"]) #, use_column_width=True, caption=vybrany_recept)
+    # Klikací odkaz na webový recept
+    # st.markdown(f"[🌐]({data['url_recept']})", unsafe_allow_html=True)
+
+    
+
+
+    # Postup
+    st.subheader("Postup")
+    st.write(data["pracovni_postup"])
+
+    if st.button("Zobrazit recept na webu"):
+        st.markdown(f'<meta http-equiv="refresh" content="0;URL={data["url_recept"]}">', unsafe_allow_html=True)
+
+    # Výživové údaje (volitelně)
+    st.subheader("Výživové hodnoty (na porci)")
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Energie (kcal)", f"{data['energie_1_porce_kcal']}")
+    col2.metric("Bílkoviny (g)", f"{data['bilkoviny_v_gramech']}")
+    col3.metric("Sacharidy (g)", f"{data['sacharidy_v_gramech']}")
+    col4.metric("Tuky (g)", f"{data['tuky_v_gramech']}")
+
+   
